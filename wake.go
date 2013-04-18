61a58dc9d4ab0896b4bcf4cca1cf682957675efa
Danny <danny@netbook.fritz.box>
2013-04-18 20:16:50 +0200

    Program for sending wol magic packets.

---
+++ wake.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ghthor/gowol"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Printf("Usage: %s <broadcast-address> <mac-address> ...\n", os.Args[0])
+		return
+	}
+	bcast := os.Args[1]
+	macs := os.Args[2:]
+	for _, m := range macs {
+		if err := wol.SendMagicPacket(m, bcast); err != nil {
+			fmt.Printf("Error for MAC '%s': '%s'\n", m, err.Error())
+		}
+	}
+}
