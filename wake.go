81e96d47f801cec2102f7756e73005d729cb1a49
Danny van Heumen <danny@dannyvanheumen.nl>
2013-04-21 00:52:22 +0200

    Lose package 'log' in favor of os.Stderr and check for existence of arguments or print usage message.

--- wake.go
+++ wake.go
@@ -3,25 +3,28 @@ package main
 import (
 	"flag"
 	"github.com/ghthor/gowol"
-	"log"
+	"os"
 )
 
 func main() {
+	if len(os.Args) <= 1 {
+		os.Stderr.WriteString("Usage: "+os.Args[0]+" -b <broadcast-address> <mac-address> [mac-address ...]\n")
+		return
+	}
 	var bcast = flag.String("b", "", "The network's broadcast address.")
 	flag.Parse()
 	if len(*bcast) <= 0 {
-		log.Printf("Please specify the network's broadcast address using the '-b' flag.")
+		os.Stderr.WriteString("Please specify the network's broadcast address using the '-b' flag.")
 		return
 	}
 	var args = flag.Args()
 	if len(args) <= 0 {
-		log.Printf("Please specify at least one MAC-address.\n")
+		os.Stderr.WriteString("Please specify at least one MAC-address.\n")
 		return
 	}
-	log.Printf("Using broadcast address: %s\n", *bcast)
 	for _, m := range args {
 		if err := wol.SendMagicPacket(m, *bcast); err != nil {
-			log.Printf("Error for MAC '%s': '%s'\n", m, err.Error())
+			os.Stderr.WriteString("Error for MAC '" + m + "': '" + err.Error() + "'\n")
 		}
 	}
 }
