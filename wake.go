9f49e55a27f2e660b778448ececb1afcd3f45111
Danny van Heumen <danny@dannyvanheumen.nl>
2013-04-21 22:41:02 +0200

    Separated initialization.

--- wake.go
+++ wake.go
@@ -1,30 +1,46 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"github.com/ghthor/gowol"
 	"os"
 )
 
 func main() {
-	if len(os.Args) <= 1 {
-		os.Stderr.WriteString("Usage: "+os.Args[0]+" -b <broadcast-address> <mac-address> [mac-address ...]\n")
+	var c *config
+	c, err := initialize()
+	if err != nil {
+		os.Stderr.WriteString("An error occurred while initializing: "+err.Error()+"\n")
 		return
+	}
+	for _, m := range c.macs {
+		if err := wol.SendMagicPacket(m, *c.bcast); err != nil {
+			os.Stderr.WriteString("Error for MAC '" + m + "': '" + err.Error() + "'\n")
+		}
+	}
+}
+
+type config struct {
+	bcast *string
+	macs []string
+}
+
+func initialize() (*config, error) {
+	var c config
+	if len(os.Args) <= 1 {
+		return nil, errors.New("Usage: "+os.Args[0]+" -b <broadcast-address> <mac-address> [mac-address ...]\n")
 	}
 	var bcast = flag.String("b", "", "The network's broadcast address.")
 	flag.Parse()
 	if len(*bcast) <= 0 {
-		os.Stderr.WriteString("Please specify the network's broadcast address using the '-b' flag.")
-		return
+		return nil, errors.New("Please specify the network's broadcast address using the '-b' flag.")
 	}
+	c.bcast = bcast
 	var args = flag.Args()
 	if len(args) <= 0 {
-		os.Stderr.WriteString("Please specify at least one MAC-address.\n")
-		return
+		return nil, errors.New("Please specify at least one MAC-address.\n")
 	}
-	for _, m := range args {
-		if err := wol.SendMagicPacket(m, *bcast); err != nil {
-			os.Stderr.WriteString("Error for MAC '" + m + "': '" + err.Error() + "'\n")
-		}
-	}
+	c.macs = args
+	return &c, nil
 }
