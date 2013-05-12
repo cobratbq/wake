feba8f62b04b8dfb4a6786a0a5b9cbb18746e45a
Danny van Heumen <danny@dannyvanheumen.nl>
2013-05-12 23:08:02 +0200

    Fixed a bug, some initialization issues and some tweaks.

--- wake.go
+++ wake.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"github.com/ghthor/gowol"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -14,7 +15,7 @@ func main() {
 		os.Stderr.WriteString(err.Error() + "\n")
 		return
 	}
-	for _, m := range c.Macs {
+	for m, _ := range c.Macs {
 		if err := wol.SendMagicPacket(m, c.Broadcast); err != nil {
 			os.Stderr.WriteString("Error for MAC '" + m + "': '" + err.Error() + "'\n")
 		} else {
@@ -25,19 +26,21 @@ func main() {
 
 type config struct {
 	Broadcast string
-	Macs      []string
+	Macs      map[string]bool
 	Profiles  map[string][]string
 }
 
 func initialize() (*config, error) {
 	var c config
+	c.Macs = make(map[string]bool)
+	c.Profiles = make(map[string][]string)
 	if err := c.load("wake.conf"); err != nil {
 		os.Stderr.WriteString("Failed to load config file 'wake.conf': " + err.Error() + "\n")
 	} else {
 		os.Stderr.WriteString("Config file loaded.\n")
 	}
 	if err := c.parseFlags(); err != nil {
-		os.Stderr.WriteString("Failed to parse flags: " + err.Error() + "\n")
+		os.Stderr.WriteString(err.Error() + "\n")
 	}
 	return &c, nil
 }
@@ -48,6 +51,7 @@ func (c *config) load(fileName string) error {
 		os.Stderr.WriteString("No config file not found or read access is not allowed.\n")
 		return err
 	}
+	defer fileReader.Close()
 	dec := json.NewDecoder(fileReader)
 	if err := dec.Decode(c); err != nil {
 		os.Stderr.WriteString("An error occurred while reading the config file: " + err.Error() + "\n")
@@ -61,20 +65,24 @@ func (c *config) parseFlags() error {
 		return errors.New("Usage: " + os.Args[0] + " -b <broadcast-address> <mac-address> [mac-address ...]\n")
 	}
 	var bcast = flag.String("b", "", "The network's broadcast address.")
-
+	// Parse the command line flags
 	flag.Parse()
-
-	if len(*bcast) <= 0 {
-		if len(c.Broadcast) <= 0 {
-			return errors.New("Please specify the network's broadcast address using the '-b' flag.")
-		}
-	} else {
+	if len(*bcast) > 0 {
 		c.Broadcast = *bcast
+	} else if len(c.Broadcast) <= 0 {
+		return errors.New("Please specify the network's broadcast address using the '-b' flag.")
 	}
 	var args = flag.Args()
 	if len(args) <= -1 {
 		return errors.New("Please specify at least one MAC-address.\n")
 	}
-	c.Macs = args
+	for _, macAddress := range args {
+		c.AddMac(macAddress)
+	}
 	return nil
+}
+
+func (c *config) AddMac(macAddress string) {
+	var mac = strings.ToLower(macAddress)
+	c.Macs[mac] = true
 }
