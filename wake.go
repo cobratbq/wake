3495805524082e987859dbccbe97e90ddd350363
Danny van Heumen <danny@dannyvanheumen.nl>
2013-05-12 21:57:34 +0200

    Initial implementation of wake.conf config file.

--- wake.go
+++ wake.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"flag"
 	"github.com/ghthor/gowol"
@@ -10,36 +11,70 @@ import (
 func main() {
 	c, err := initialize()
 	if err != nil {
-		os.Stderr.WriteString(err.Error()+"\n")
+		os.Stderr.WriteString(err.Error() + "\n")
 		return
 	}
-	for _, m := range c.macs {
-		if err := wol.SendMagicPacket(m, *c.bcast); err != nil {
+	for _, m := range c.Macs {
+		if err := wol.SendMagicPacket(m, c.Broadcast); err != nil {
 			os.Stderr.WriteString("Error for MAC '" + m + "': '" + err.Error() + "'\n")
+		} else {
+			os.Stdout.WriteString("Waking '" + m + "' ...\n")
 		}
 	}
 }
 
 type config struct {
-	bcast *string
-	macs []string
+	Broadcast string
+	Macs      []string
+	Profiles  map[string][]string
 }
 
 func initialize() (*config, error) {
 	var c config
+	if err := c.load("wake.conf"); err != nil {
+		os.Stderr.WriteString("Failed to load config file 'wake.conf': " + err.Error() + "\n")
+	} else {
+		os.Stderr.WriteString("Config file loaded.\n")
+	}
+	if err := c.parseFlags(); err != nil {
+		os.Stderr.WriteString("Failed to parse flags: " + err.Error() + "\n")
+	}
+	return &c, nil
+}
+
+func (c *config) load(fileName string) error {
+	fileReader, err := os.Open(fileName)
+	if err != nil {
+		os.Stderr.WriteString("No config file not found or read access is not allowed.\n")
+		return err
+	}
+	dec := json.NewDecoder(fileReader)
+	if err := dec.Decode(c); err != nil {
+		os.Stderr.WriteString("An error occurred while reading the config file: " + err.Error() + "\n")
+		return err
+	}
+	return nil
+}
+
+func (c *config) parseFlags() error {
 	if len(os.Args) <= 1 {
-		return nil, errors.New("Usage: "+os.Args[0]+" -b <broadcast-address> <mac-address> [mac-address ...]\n")
+		return errors.New("Usage: " + os.Args[0] + " -b <broadcast-address> <mac-address> [mac-address ...]\n")
 	}
 	var bcast = flag.String("b", "", "The network's broadcast address.")
+
 	flag.Parse()
+
 	if len(*bcast) <= 0 {
-		return nil, errors.New("Please specify the network's broadcast address using the '-b' flag.")
+		if len(c.Broadcast) <= 0 {
+			return errors.New("Please specify the network's broadcast address using the '-b' flag.")
+		}
+	} else {
+		c.Broadcast = *bcast
 	}
-	c.bcast = bcast
 	var args = flag.Args()
-	if len(args) <= 0 {
-		return nil, errors.New("Please specify at least one MAC-address.\n")
+	if len(args) <= -1 {
+		return errors.New("Please specify at least one MAC-address.\n")
 	}
-	c.macs = args
-	return &c, nil
+	c.Macs = args
+	return nil
 }
